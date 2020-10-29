ad0d94af2996b640967bb2d2e5354a626fe914ee
G.Marinenko <Gmarinenko@mail.ru>
2020-10-29 10:16:56 +0300

    ft_memcmp

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: georgy <georgy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 22:43:07 by georgy            #+#    #+#             */
-/*   Updated: 2020/10/28 23:29:44 by georgy           ###   ########.fr       */
+/*   Updated: 2020/10/29 00:33:08 by georgy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,28 @@ void *ft_memchr (const void *arr, int c, size_t b_size)
 	return (NULL);
 }
 
+int	ft_memcmp(const void *arr1, const void *arr2, size_t b_size)
+{
+	size_t	i;
+	char	*p_arr1;
+	char	*p_arr2;
+
+	p_arr1 = (char*)arr1;
+	p_arr2 = (char*)arr2;
+	i = 0;
+	while (i <= b_size)
+	{
+		printf("arr1[%zu]: %c\t\t", i, p_arr1[i]);
+		printf("arr2[%zu]: %c\n", i, p_arr2[i]);
+		if (p_arr1[i] < p_arr2[i])
+			return ((p_arr1 - p_arr2)*(-1));
+		else if (p_arr1[i] > p_arr2[i])
+			return (p_arr1 - p_arr2);
+		i++;
+	}
+	return (0);
+}
+
 int	main(void)
 {
 	char	src_orig[15] = "1234567890";
@@ -73,7 +95,14 @@ int	main(void)
 	sym = ft_memchr(src_chr, '9', 9);
 	if (sym != NULL)
 		sym[0]= '!';
-
 	printf("new arr: %s\n", src_chr);
+
+	char	arr1[15] = "123546";
+	char	arr2[15] = "123357";
+	int		rez;
+
+
+	rez = ft_memcmp(arr1, arr2, 4);
+	printf("\nmemcmp rez: %d\n", rez);
 	return (0);
 }
