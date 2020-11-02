3c729ac0cba26965ae41bee11854196c681b9b8e
G.Marinenko <Gmarinenko@mail.ru>
2020-11-03 01:05:17 +0300

    ft_itoa +++++

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: georgy <georgy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 23:52:09 by georgy            #+#    #+#             */
-/*   Updated: 2020/11/03 00:39:07 by georgy           ###   ########.fr       */
+/*   Updated: 2020/11/03 01:02:45 by georgy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,28 +43,31 @@ char *ft_itoa(int n)
 	else
 		minus = 0;
 	numb_len = cnt_rank_number(n, minus);
+	//printf("\n numb_len = %d\n", numb_len);
 	res = (char *)malloc(sizeof(char) * (numb_len + 1));
 	if (res == NULL)
 		return NULL;
-	res[numb_len - 1] = '\0';
+	res[numb_len] = '\0';
 	while (numb_len >= minus)
 	{
 		digit = n % 10;
-		res[numb_len--] = ft_abs(digit) + '0';
+		numb_len--;
+		res[numb_len] = abs(digit) + '0';
 		n /= 10;
 	}
-	if (minus < 0)
+	if (minus > 0)
 		res[0] = '-';
 	return (res);
 }
 
-int main(void)
-{
-	char	*i1 = ft_itoa(-62);
-	char	*i2 = ft_itoa(156);
-	char	*i3 = ft_itoa(-0);
+// int main(void)
+// {
+// 	char	*i1 = ft_itoa(-62);
+// 	char	*i2 = ft_itoa(156);
+// 	char	*i3 = ft_itoa(-1);
 
-	printf("\n i1 = %s\n", i1);
-	printf("\n i2 = %s\n", i2);
-	printf("\n i3 = %s\n", i3);
-}
+// 	printf("\n i1 = %s\n", i1);
+// 	printf("\n i2 = %s\n", i2);
+// 	printf("\n i3 = %s\n", i3);
+// 	//printf("\n numb_len = %d\n", cnt_rank_number(156, 0));
+// }
