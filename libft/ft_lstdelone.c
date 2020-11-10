8877deb5ae6a3561cdfcdca4846ce1fd4f0afbac
Георгий Мариненко <aarlena@ox-u1.msk.21-school.ru>
2020-11-10 20:50:49 +0300

    retry 1 v1.2.0

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: aarlena <aarlena@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:02:52 by aarlena           #+#    #+#             */
-/*   Updated: 2020/11/04 19:15:57 by aarlena          ###   ########.fr       */
+/*   Updated: 2020/11/10 20:47:21 by aarlena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	if (lst)
-	{
-		(*del)((lst->content));
-		free(lst);
-		lst->next = NULL;
-	}
+	if (!lst)
+		return ;
+	(*del)((lst->content));
+	free(lst);
 }
